f5d267eda8afe0f2332565f80fd8360ffbc3990d
shiruco <shiruco621@gmail.com>
2019-05-03 22:28:30 +0900

    circle ci

--- test/sample_token_test.js
+++ test/sample_token_test.js
@@ -1,9 +1,13 @@
 const SampleToken = artifacts.require('SampleToken.sol');
 
 contract('SampleToken', function(accounts) {
-  it("...should token version is v1.", async () => {
-    const sampleTokenInstance = await SampleToken.deployed();
+  const sampleTokenInstance;
 
+  beforeEach(async function() {
+    sampleTokenInstance = await SampleToken.deployed();
+  });
+
+  it("...should token version is v1.", async () => {
     let version = await sampleTokenInstance.version();
     assert.equal(version, "v1");
   });
